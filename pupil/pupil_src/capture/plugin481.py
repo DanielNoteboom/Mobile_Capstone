b78537067b799e8ab493cdf797b7e911f9cf68fb
Daniel Noteboom <djnoboom@cs.washington.edu>
2015-05-03 16:56:57 -0700

    added mock gui

--- pupil/pupil_src/capture/plugin481.py
+++ pupil/pupil_src/capture/plugin481.py
@@ -1,10 +1,10 @@
-
+import cv2
 from plugin import Plugin
 import logging
 logger = logging.getLogger(__name__)
-
+import sys
 logger.warning("in Plugin file")
-
+count = 0
 class ClickDetect(Plugin):
   def __init__(self, g_pool):
     super(ClickDetect, self).__init__(g_pool)
@@ -15,4 +15,13 @@ class ClickDetect(Plugin):
   def on_click(self, pos, button,action):
     logger.warning('Click thing')
   def update(self, frame, events):
-  	logger.warning("In update")
+    global count
+   
+    cv2.imwrite("pic/pic" + str(count) + ".jpg", frame.img)
+    text = open("pic/pic" + str(count) + ".txt", 'w')
+    f = open("pic/current_count.txt", 'w')
+    f.write(str(count))
+    for g in events.get('gaze', []):
+      text.write("x " + str(g['norm_pos'][0]) + " y " + str(g['norm_pos'][1]))
+
+    count += 1
