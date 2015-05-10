df0b3711826e47502d1b6f7de05d8ca31cc89103
Evan Whitfield <evan.whitfield@gmail.com>
2015-05-09 21:07:33 -0700

    Adding panels to gui

--- gui/gui.py
+++ gui/gui.py
@@ -8,9 +8,9 @@ import os
 import time
 
 
-
 from PIL import Image, ImageTk
-from Tkinter import Tk, W,E, Label, BOTH, RIGHT, RAISED
+from Tkinter import Tk, N,S,W,E, Label, BOTH, RIGHT, RAISED, LEFT
+from Tkinter import TOP, BOTTOM
 from ttk import Frame, Style, Button
 from ttk import Entry
 
@@ -33,7 +33,7 @@ class Example(Frame):
                       text="GAZE CAPTURED", fill="white")
 
     def initUI(self):
-      run_pupil()
+      #run_pupil()
 
       #  Defining a method for absolute positioning of an image
       def place_img(self, filename, x, y):
@@ -69,8 +69,44 @@ class Example(Frame):
         print "Not implemented"
 
 
+      # Top box
+      upper_frame = Frame(self, relief=RAISED )
+      upper_frame.grid(row = 0, column = 0, columnspan=2, sticky=N+E+S+W)
+
+      # Creates a panel in the frame passed in, and returns a list of frame objects
+      #  that need to be accessed in the panel
+      def make_panel(panel_frame):
+        ### Panel 1
+        p1 = Frame(upper_frame, relief=RAISED, borderwidth =1)
+        p1.pack(side = TOP, fill = BOTH, expand=1)
+
+        pic_frame1_1 = Frame(p1, relief=RAISED, borderwidth =1)
+        pic_frame1_1.pack(side = LEFT, fill = BOTH, expand=1,padx=15, pady=4)
+        pic1_1 = Frame(pic_frame1_1, relief=RAISED, borderwidth =1)
+        pic1_1.pack(side = TOP, fill = BOTH, expand=1)
+        label1_1 = Label(pic_frame1_1, relief=RAISED, borderwidth =1, text = "Captured face")
+        label1_1.pack(side = BOTTOM, fill = BOTH)
+
+        pic_frame1_2 = Frame(p1, relief=RAISED, borderwidth =1)
+        pic_frame1_2.pack(side = LEFT, fill = BOTH, expand=1,padx=15, pady=4)
+        pic1_2 = Frame(pic_frame1_2, relief=RAISED, borderwidth =1)
+        pic1_2.pack(side = TOP, fill = BOTH, expand=1)
+        label1_2 = Label(pic_frame1_2, relief=RAISED, borderwidth =1, text = "Best match")
+        label1_2.pack(side = BOTTOM, fill = BOTH)
+
+        side_panel1 = Frame(p1, relief=RAISED, borderwidth =1)
+        side_panel1.pack(side = RIGHT, fill = BOTH, expand=1)
+
+        return [pic_frame1_1]
+
+
       load = Label(self, text="Loading...", background="#eee")
       load.grid(row=0,column=0, columnspan=2)
+
+      #  make 3 panels
+      make_panel(upper_frame)
+      make_panel(upper_frame)
+      make_panel(upper_frame)
 
       b2 = Button(self, text="Focus camera", command=other)
       b2.grid(row=1,column=0)
@@ -78,8 +114,8 @@ class Example(Frame):
       cb.grid(row=1,column=1)
 
 
-      # some kind of hack to bring this window to the front as it is launched. Probably
-      #  doesn't work on windows.
+      # Some kind of hack to bring this window to the front as it is launched. 
+      #  Won't work on windows.
       if os.name == "posix":
         os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
 
@@ -87,7 +123,6 @@ def main():
     root = Tk()
     app = Example(root)
     root.mainloop()  
-
 
 if __name__ == '__main__':
     main()  
