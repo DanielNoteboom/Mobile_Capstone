175022d7b3855eaf45629f924778c1b8ba4e1d2c
DJu90 <dju90@uw.edu>
2015-05-14 12:29:34 -0700

    bug fix for incorrect path on the best match image

--- face_comparison/compare.py
+++ face_comparison/compare.py
@@ -27,27 +27,44 @@ def compare( test, cDir ):
   data = compOutput.communicate()
   # data is contained in fist index of output
   dataArray = str(data[0]).split()
-  extCtr = 0
-  scores = {}
+  scores = []
   for i in range(len(dataArray)):
     if i % 2 == 0 and i != 0:
-      scores[counter] = float(dataArray[i])
-      extCtr += 1
+      scores.append(float(dataArray[i]))
 
+  matches = getMatches(cDir, scores)
+
+  hits = []
+  for i in range(NUM_MATCHES):
+      try:
+          hit = matches.get_nowait()
+          hits.append({
+            'matchPath': hit[2], 
+            'id': hit[1].split("/")[-1:][0], 
+            'average': -hit[0],
+            'median': -hit[3]
+            })
+      except Queue.Empty:
+      	break
+  return hits
+
+def getMatches( cDir, scores ):
   matches = Queue.PriorityQueue(0)
-  idCtr = 0 # index in the large array
+  extCtr = 0 # index in the large array
   comparisons = listdir(cDir)
   # only works because both OpenBR and python recurse thru dirs alphanumerically
   for identity in comparisons:
-    idScores = []
-    max = 1.0
     if os.path.isdir(cDir + "/" + identity):
+      idScores = []
+      max = 1.0
+
       # aggregate all scores for a given id
       images = listdir(cDir + "/" + identity)
       numImages = len(images)
       bestImage = 1
       for i in range(numImages):
-        score = scores[idCtr]
+        score = scores[extCtr]
+        extCtr += 1
         if score > max:
           max = score
           bestImage = i
@@ -56,37 +73,24 @@ def compare( test, cDir ):
                   os.path.abspath(cDir + "/" + identity), #id
                   os.path.abspath(cDir + "/" + identity + "/" + images[bestImage]), #bestImage.jpg
                   -sorted(idScores)[numImages/2])) #median
+  return matches
 
-  # for identity in comparisons:
-  #   if os.path.isdir(cDir + "/" + identity):
-  #     # find openBR correlation for each image in subdirectory    
-  #     cImages = listdir(cDir + "/" + identity)
-  #     aggregateIndex = 0
-  #     compOutput = Popen(["br", "-algorithm", "FaceRecognition", "-compare", 
-  #                         test, cDir + "/" + identity],
-  #                         stdout=PIPE, stderr=PIPE)
-  #     data = compOutput.communicate()
-  #     dataArray = str(data[0]).split()
-  #     source = str(data[1]).split()[9].split("/")[1]
-  #     for i in range(len(dataArray)):
-  #       if i % 2 == 0 and i != 0:
-  #         aggregateIndex += float(dataArray[i].strip())
-  #     # (-) appended b/c min-queue, need max aggregateIndex.
-  #     matches.put((-aggregateIndex, os.path.abspath(cDir + "/" + identity)))
-
-  hits = []
-  for i in range(NUM_MATCHES):
-      try:
-          hit = matches.get_nowait()
-          hits.append({
-            'matchPath': hit[3], 
-            'id': hit[1].split("/")[-1:][0], 
-            'average': -hit[0],
-            'median': -hit[3]
-            })
-      except Queue.Empty:
-      	break
-  return hits
+def compareByDir(cDir, comparisons): #tooSlow
+  matches = Queue.PriorityQueue(0)
+  for identity in comparisons:
+    if os.path.isdir(cDir + "/" + identity):  
+      cImages = listdir(cDir + "/" + identity)
+      aggregateIndex = 0
+      compOutput = Popen(["br", "-algorithm", "FaceRecognition", "-compare", 
+                          test, cDir + "/" + identity],
+                          stdout=PIPE, stderr=PIPE)
+      data = compOutput.communicate()
+      dataArray = str(data[0]).split()
+      source = str(data[1]).split()[9].split("/")[1]
+      for i in range(len(dataArray)):
+        if i % 2 == 0 and i != 0:
+          aggregateIndex += float(dataArray[i].strip())
+      matches.put((-aggregateIndex, os.path.abspath(cDir + "/" + identity)))
 
 if __name__ == "__main__":
   if len(sys.argv) != 3:
