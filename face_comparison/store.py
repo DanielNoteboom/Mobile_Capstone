af42d59b1a14281c98bf265f06fd167201b53bb7
DJu90 <dju90@uw.edu>
2015-05-19 12:53:47 -0700

    now can store an image in the image store

---
+++ face_comparison/store.py
@@ -0,0 +1,25 @@
+from os import listdir
+import sys
+import os.path
+import shutil
+
+#controls the number of matches that the code will return
+IMAGE_DIR = "c1"
+
+# stores the parameter image in the comparison folder
+def add_training_data( image, id ):
+  id.replace(" ", "_")
+  identities = listdir(IMAGE_DIR)
+  if id in identities:
+    images = listdir(IMAGE_DIR + "/" + id)
+    shutil.copyfile(image, os.path.join(IMAGE_DIR + "/" + id, str(len(images)+1)+".jpg"))
+  else:
+    print id + " does not exist in our records!"
+    sys.exit(0)
+
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print "Usage: python store.py IMAGE_PATH IMAGE_ID"
+    sys.exit(0)
+  else:
+    add_training_data(sys.argv[1], sys.argv[2])
