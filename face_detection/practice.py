0efe99391623a27ddd44e9d469d926760358648b
Evan Whitfield <evan.whitfield@gmail.com>
2015-05-18 23:27:48 -0700

    Adding 'practice' script for face detection

---
+++ face_detection/practice.py
@@ -0,0 +1,33 @@
+import sys
+from PIL import Image
+
+from face import get_top_matches
+
+if len(sys.argv) > 1:
+  img = sys.argv[1]
+else:
+  img = 'img1.jpg'
+
+if len(sys.argv) > 2:
+  scale_factor = float(sys.argv[2])
+else:
+  scale_factor = 1.2
+
+if len(sys.argv) > 3:
+  min_neighbors = int(sys.argv[3])
+else:
+  min_neighbors = 4
+
+#   run facial detetction on arguments passed in
+
+get_top_matches(img, 0,0,True, scale_factor, min_neighbors)
+
+#  we save the images with rectangles to 'boxes.jpg' in face.py
+  
+image = Image.open('boxes.jpg')
+image.show()
+
+
+
+
+
